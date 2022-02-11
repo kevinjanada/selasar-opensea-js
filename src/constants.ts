e0207e25b3bdcd38a2c3f195303a73bdb122aeda
Kevin Janada <kevin.janada@gmail.com>
2022-02-11 09:57:22 +0700

    Update post order api endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -61,5 +61,6 @@ export const SITE_HOST_RINKEBY = "https://rinkeby.opensea.io";
 export const RPC_URL_PATH = "jsonrpc/v1/";
 export const MAINNET_PROVIDER_URL = `${API_BASE_MAINNET}/${RPC_URL_PATH}`;
 export const RINKEBY_PROVIDER_URL = `${API_BASE_RINKEBY}/${RPC_URL_PATH}`;
-export const ORDERBOOK_PATH = `/wyvern/v${ORDERBOOK_VERSION}`;
+//export const ORDERBOOK_PATH = `/wyvern/v${ORDERBOOK_VERSION}`;
+export const ORDERBOOK_PATH = `/api/v${ORDERBOOK_VERSION}`;
 export const API_PATH = `/api/v${ORDERBOOK_VERSION}`;
